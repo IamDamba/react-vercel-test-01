629b9d445b3b3790c182ef1c46855efb7c1ea375
iamdamba <iamdamba720@gmail.com>
2022-09-09 11:50:19 +0200

    add commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
         target: "http://localhost:3001/api",
       },
     },
+  },
+  build: {
+    outDir: path.join(__dirname, "..//vercel/output"),
   },
 });
